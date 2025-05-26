f4a95f67ed73c3f47c21c4601738dd0408fb7545
NeoCircuit <154694006+NeoCircuit-Studios@users.noreply.github.com>
2025-05-26 20:38:59 +0200

    Add files via upload

---
+++ Build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "      NeoCircuit-Studios (NS)"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo ""
+
+echo "V0.1.3"
+echo ""
+echo "Upgrading.." # remove this old shit lol
+
+sudo wget https://github.com/NeoCircuit-Studios/wine/releases/download/sys/NS-Build.sh
+sudo wget https://github.com/NeoCircuit-Studios/Wine-Tools/releases/download/sys/NS-Build.sh
+
+
+dos2unix NS-Build.sh
+
+sudo chmod +x NS-Build.sh
+
+sudo ./NS-Build.sh
+
